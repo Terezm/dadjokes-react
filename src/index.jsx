23cbcff8e1351edcb1324d029bccb459e1a559e8
Tereza Maninová <maninova69@gmail.com>
2023-06-03 14:51:03 +0200

    funkce handleClick

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 
 const App = () => {
+
+ 
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0)
+  
+
+const handleUpClick = () => {
+  setLike(like + 1);
+}
+
+const handleDownClick = () => {
+  setDislike(dislike + 1);
+}
+
   return (
     <div className="container">
       
@@ -21,9 +35,9 @@ const App = () => {
           </p>
         </div>
         <div className="joke__likes">
-          <button id="btn-up" className="btn-like btn-like--up"></button>
+          <button onClick={handleUpClick} id="btn-up" className="btn-like btn-like--up"></button>
           <span id="likes-up" className="likes-count likes-count--up">0</span>
-          <button id="btn-down" className="btn-like btn-like--down"></button>
+          <button onClick={handleDownClick}  id="btn-down" className="btn-like btn-like--down"></button>
           <span id="likes-down" className="likes-count likes-count--down">0</span>
         </div>
       </div>
